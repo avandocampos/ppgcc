fd6fe5a5a2854640ad3d13ce161c353dd0e21335
Avando Campos <avandocampos@gmail.com>
2024-03-24 20:35:55 -0300

    first implementation of media filter

--- S1/PDI/T1/filtro_passa_baixa/media.py
+++ S1/PDI/T1/filtro_passa_baixa/media.py
@@ -6,10 +6,11 @@ def filtro_passa_baixa_media(imagem, tamanho_kernel):
     return cv2.blur(imagem, (tamanho_kernel, tamanho_kernel))
 
 
-# Exemplo de uso:
-imagem = cv2.imread('imagem.png', 0)  # Carregar imagem em escala de cinza
-imagem_filtrada_media = filtro_passa_baixa_media(imagem, 5)  # Aplicar filtro com kernel 5x5
-cv2.imshow('Imagem Original', imagem)
-cv2.imshow('Imagem Filtrada (Média)', imagem_filtrada_media)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+
+    imagem = cv2.imread('imagem.png', 0)  # Carregar imagem em escala de cinza
+    imagem_filtrada_media = filtro_passa_baixa_media(imagem, 5)  # Aplicar filtro com kernel 5x5
+    cv2.imshow('Imagem Original', imagem)
+    cv2.imshow('Imagem Filtrada (Média)', imagem_filtrada_media)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
