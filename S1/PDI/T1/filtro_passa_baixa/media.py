ff253568ce048af7b8fcb06264b51026fec18542
Avando Campos <avandocampos@gmail.com>
2024-03-24 21:20:38 -0300

    translated code to english

--- S1/PDI/T1/filtro_passa_baixa/media.py
+++ S1/PDI/T1/filtro_passa_baixa/media.py
@@ -1,23 +1,23 @@
 import cv2
 
 
-def filtro_passa_baixa_media(imagem, tamanho_kernel):
-    altura, largura = imagem.shape
-    imagem_filtrada = imagem.copy().astype(float)
-    offset = tamanho_kernel // 2
+def low_pass_filter_average(image, kernel_size):
+    height, width = image.shape
+    filtered_image = image.copy().astype(float)
+    offset = kernel_size // 2
 
-    for y in range(offset, altura - offset):
-        for x in range(offset, largura - offset):
-            sub_imagem = imagem[y - offset:y + offset + 1, x - offset:x + offset + 1]
-            imagem_filtrada[y, x] = (sub_imagem.sum() / (tamanho_kernel ** 2)).astype('uint8')
+    for y in range(offset, height - offset):
+        for x in range(offset, width - offset):
+            sub_image = image[y - offset:y + offset + 1, x - offset:x + offset + 1]
+            filtered_image[y, x] = (sub_image.sum() / (kernel_size ** 2)).astype('uint8')
 
-    cv2.normalize(imagem_filtrada, imagem_filtrada, 0, 255, cv2.NORM_MINMAX)
+    cv2.normalize(filtered_image, filtered_image, 0, 255, cv2.NORM_MINMAX)
 
-    return imagem_filtrada.astype('uint8')
+    return filtered_image.astype('uint8')
 
 
 if __name__ == '__main__':
 
-    imagem = cv2.imread('imagem.jpeg', 0)  # Carregar imagem em escala de cinza
-    imagem_filtrada_media = filtro_passa_baixa_media(imagem, 5)  # Aplicar filtro com kernel 5x5
-    cv2.imwrite('imagem_filtrada_media.jpeg', imagem_filtrada_media)
+    image = cv2.imread('imagem.jpeg', 0)  # Load image in grayscale
+    filtered_image_average = low_pass_filter_average(image, 5)  # Apply filter with 5x5 kernel
+    cv2.imwrite('filtered_image_average.jpeg', filtered_image_average)
